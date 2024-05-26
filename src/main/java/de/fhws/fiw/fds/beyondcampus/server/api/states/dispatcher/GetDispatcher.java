8d5af9a38bf6e87ce90d5d70750194db76e90068
YashMehta11k <yashmht44@gmail.com>
2024-05-27 00:02:16 +0200

    Dispatcher states and transition links created

---
+++ src/main/java/de/fhws/fiw/fds/beyondcampus/server/api/states/dispatcher/GetDispatcher.java
@@ -0,0 +1,30 @@
+package de.fhws.fiw.fds.beyondcampus.server.api.states.dispatcher;
+
+import de.fhws.fiw.fds.beyondcampus.server.api.states.modules.ModuleRelTypes;
+import de.fhws.fiw.fds.beyondcampus.server.api.states.modules.ModuleUri;
+import de.fhws.fiw.fds.beyondcampus.server.api.states.partnerunis.PartnerUniRelTypes;
+import de.fhws.fiw.fds.beyondcampus.server.api.states.partnerunis.PartnerUniUri;
+import de.fhws.fiw.fds.sutton.server.api.serviceAdapters.responseAdapter.JerseyResponse;
+import de.fhws.fiw.fds.sutton.server.api.services.ServiceContext;
+import de.fhws.fiw.fds.sutton.server.api.states.get.AbstractGetDispatcherState;
+import jakarta.ws.rs.core.Response;
+
+public class GetDispatcher extends AbstractGetDispatcherState<Response> {
+
+    public GetDispatcher(ServiceContext serviceContext){
+        super(serviceContext);
+        this.suttonResponse=new JerseyResponse<>();
+    }
+
+    @Override
+    protected void defineTransitionLinks(){
+
+        addLink(PartnerUniUri.REL_PATH,
+                PartnerUniRelTypes.GET_ALL_PARTNERUNIS,
+                getAcceptRequestHeader());
+
+        addLink(ModuleUri.REL_PATH,
+                ModuleRelTypes.GET_ALL_MODULES,
+                getAcceptRequestHeader());
+    }
+}
