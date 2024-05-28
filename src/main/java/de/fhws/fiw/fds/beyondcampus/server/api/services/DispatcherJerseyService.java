ed41b30100986d8208a6672ad1e46a69ad065043
YashMehta11k <yashmht44@gmail.com>
2024-05-28 02:20:28 +0200

    Dispatcher , PartnerUni and Module Jersey service created

---
+++ src/main/java/de/fhws/fiw/fds/beyondcampus/server/api/services/DispatcherJerseyService.java
@@ -0,0 +1,34 @@
+package de.fhws.fiw.fds.beyondcampus.server.api.services;
+
+import de.fhws.fiw.fds.beyondcampus.server.api.states.dispatcher.GetDispatcher;
+import de.fhws.fiw.fds.beyondcampus.server.database.utils.ResetDatabase;
+import de.fhws.fiw.fds.sutton.server.api.serviceAdapters.Exceptions.SuttonWebAppException;
+import de.fhws.fiw.fds.sutton.server.api.services.AbstractJerseyService;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("")
+public class DispatcherJerseyService extends AbstractJerseyService {
+
+    @GET
+    public Response getDispatcher(){
+        try{
+            return new GetDispatcher(this.serviceContext).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @GET
+    @Path("resetdatabase")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response resetDatabase(){
+        System.out.println("Resetting database");
+        ResetDatabase.resetAll();
+        return Response.ok().build();
+    }
+}
---
+++ src/main/java/de/fhws/fiw/fds/beyondcampus/server/api/services/PartnerUniJerseyService.java
@@ -0,0 +1,141 @@
+package de.fhws.fiw.fds.beyondcampus.server.api.services;
+
+import de.fhws.fiw.fds.beyondcampus.server.api.models.Module;
+import de.fhws.fiw.fds.beyondcampus.server.api.models.PartnerUni;
+import de.fhws.fiw.fds.beyondcampus.server.api.queries.QueryByModName;
+import de.fhws.fiw.fds.beyondcampus.server.api.queries.QueryByUniName;
+import de.fhws.fiw.fds.beyondcampus.server.api.states.partnerUniModules.*;
+import de.fhws.fiw.fds.beyondcampus.server.api.states.partnerunis.*;
+import de.fhws.fiw.fds.sutton.server.api.serviceAdapters.Exceptions.SuttonWebAppException;
+import de.fhws.fiw.fds.sutton.server.api.services.AbstractJerseyService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("partnerunis")
+public class PartnerUniJerseyService extends AbstractJerseyService {
+
+    public PartnerUniJerseyService(){
+        super();
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response getAllPartnerUnis(
+            @DefaultValue("") @QueryParam("uniName") final String uniName,
+            @DefaultValue("") @QueryParam("uniCountry") final String uniCountry,
+            @DefaultValue("") @QueryParam("departmentName") final String departmentName,
+            @DefaultValue("0") @QueryParam("offset") int offset,
+            @DefaultValue("20") @QueryParam("size") int size){
+
+        try {
+            return new GetAllPartnerUnis(
+                    this.serviceContext,
+                    new QueryByUniName<>(uniName, offset, size)
+            ).execute();
+        }catch (SuttonWebAppException e){
+            throw new WebApplicationException(e.getExceptionMessage(),e.getStatus().getCode());
+        }
+    }
+
+    @GET
+    @Path("{id:\\d+}")
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response getSinglePartnerUni(@PathParam("id") final long id){
+        try{
+            return new GetSinglePartnerUni(this.serviceContext,id).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public Response createSinglePartnerUni(final PartnerUni partnerUniModel){
+        try{
+            return new PostNewPartnerUni(this.serviceContext,partnerUniModel).execute();
+        }catch (SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @PUT
+    @Path("{id:\\d+}")
+    @Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response updateSinglePartnerUni(@PathParam("id") final long id,final PartnerUni partnerUniModel){
+
+        try{
+            return new PutSinglePartnerUni(this.serviceContext,id,partnerUniModel).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @DELETE
+    @Path("{id:\\d+}")
+    @Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response deleteSinglePartnerUni(@PathParam("id") final long id){
+        try{
+            return new DeleteSinglePartnerUni(this.serviceContext,id).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @GET
+    @Path("{partneruniId:\\d+}/modules")
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response getModulesOfPartnerUni(@PathParam("partneruniId") final long partneruniId,
+                                           @DefaultValue("") @QueryParam("modName") final String modName,
+                                           @DefaultValue("0") @QueryParam("offset") int offset,
+                                           @DefaultValue("20") @QueryParam("size") int size){
+        try {
+            return new GetAllModulesofPartnerUni(this.serviceContext,partneruniId,new QueryByModName<>(partneruniId,modName,offset,size)).execute();
+        }catch (SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @GET
+    @Path("{partneruniId:\\d+}/modules/{moduleId:\\d+}")
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response getModuleByIdOfPartnerUni(@PathParam("partneruniId") final long partneruniId,@PathParam("moduleId")final long moduleId){
+        try{
+            return new GetSingleModuleofPartnerUni(this.serviceContext,partneruniId,moduleId).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @POST
+    @Path("{partneruniId:\\d+}/modules")
+    @Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response createNewModuleOfPartnerUni(@PathParam("partneruniId") final long partneruniId, final Module module){
+        try{
+            return new PostNewModuleOfPartnerUni(this.serviceContext,partneruniId,module).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @PUT
+    @Path("{partneruniId:\\d+}/modules/{moduleId:\\d+}")
+    @Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response updateNewModuleOfPartnerUni(@PathParam("partneruniId") final long partneruniId,@PathParam("moduleId")final long moduleId,final Module module){
+        try{
+            return new PutSingleModuleOfPartnerUni(this.serviceContext,partneruniId,moduleId,module).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+
+    @DELETE
+    @Path("{partneruniId:\\d+}/modules/{moduleId:\\d+}")
+    public Response deleteModuleOfPartnerUni(@PathParam("partneruniId") final long partneruniId,@PathParam("moduleId")final long moduleId){
+        try{
+            return new DeleteSingleModuleOfPartnerUni(this.serviceContext,partneruniId,moduleId).execute();
+        }catch(SuttonWebAppException e){
+            throw new WebApplicationException(Response.status(e.getStatus().getCode()).entity(e.getExceptionMessage()).build());
+        }
+    }
+}
